fdd92fb8c3f835eeb558c7543200feae26b8ca55
Tony <xhan9966@conestogac.on.ca>
2022-07-20 11:30:10 -0400

    part 1.

--- task_list/task_list.js
+++ task_list/task_list.js
@@ -1,30 +1,39 @@
 "use strict";
 
-$(document).ready( () => {
-    const tasks = [];
+$(document).ready(() => {
+  const tasks = [];
 
-    $("#add_task").click( () => {   
-        const textbox = $("#task");
-        const task = textbox.val();
-        if (task === "") {
-            alert("Please enter a task.");
-            textbox.focus();
-        } else {
-            // add task to array
-            tasks.push(task);
-
-            // clear task text box and re-display tasks
-            textbox.val("");
-            $("#task_list").val(tasks.join("\n"));
-            textbox.focus();
+  $("#add_task").click(() => {
+    const textbox = $("#task");
+    const task = textbox.val();
+    if (task === "") {
+      alert("Please enter a task.");
+      textbox.focus();
+    } else {
+      // add task to array
+      if (task.indexOf(",") != -1) {
+        let newTasks = task.split(",");
+        for (let t of newTasks) {
+          if (t !== "") {
+            tasks.push(t);
+          }
         }
-    });
-    
-    $("#clear_tasks").click( () => {
-        tasks.length = 0;
-        $("#task_list").val("");
-        $("#task").focus();
-    }); 
-    
+      } else {
+        tasks.push(task);
+      }
+
+      // clear task text box and re-display tasks
+      textbox.val("");
+      $("#task_list").val(tasks.join("\n"));
+      textbox.focus();
+    }
+  });
+
+  $("#clear_tasks").click(() => {
+    tasks.length = 0;
+    $("#task_list").val("");
     $("#task").focus();
+  });
+
+  $("#task").focus();
 });
